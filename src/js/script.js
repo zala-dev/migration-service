4c252edf0b0a4c02d1566a23a3af1f61f97c035a
zala-dev <zala.developer@gmail.com>
2022-01-04 23:06:24 +1030

    fixed logo

--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,26 @@
 'use strict';
-
+const navMobileContainer = document.querySelector('.nav-mobile');
+const navMobileList = document.querySelector('.nav-mobile-list');
+const navMobileLink = document.querySelector('.nav-mobile-link');
 const openMenuContainer = document.getElementById('nav-mobile-open-container');
 const closeMenuContainer = document.getElementById(
   'nav-mobile-close-container'
 );
 
 openMenuContainer.addEventListener('click', (e) => {
-  console.log(e.target);
   if (e.target.classList.contains('fas')) {
-    document.querySelector('.nav-mobile').classList.remove('hidden');
+    navMobileContainer.classList.remove('hidden');
   }
 });
 
 closeMenuContainer.addEventListener('click', (e) => {
   if (e.target.classList.contains('far')) {
-    document.querySelector('.nav-mobile').classList.add('hidden');
+    navMobileContainer.classList.add('hidden');
   }
+});
+
+navMobileContainer.addEventListener('click', (e) => {
+  const navLink = e.target.closest('#nav-link');
+  if (!navLink) return;
+  navMobileContainer.classList.add('hidden');
 });
